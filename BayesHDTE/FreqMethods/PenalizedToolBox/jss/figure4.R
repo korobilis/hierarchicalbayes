65fc8b8d135b826e29449adef57d3c64ce87b2f9
Dimitris Korobilis <54067194+korobilis@users.noreply.github.com>
2025-06-04 19:21:02 +0100

    Add files via upload

---
+++ BayesHDTE/FreqMethods/PenalizedToolBox/jss/figure4.R
@@ -0,0 +1,24 @@
+library("R.matlab")
+library("glmnet")
+
+a <- readMat("jssfigure4.mat")
+if (a$standardize) {
+    message("Standardized")
+}
+if (a$intercept) {
+    message("Intercept")
+}
+x <- proc.time()
+fit <- glmnet(a$X, a$y, family = a$type, standardize = a$standardize, intercept = a$intercept,
+              alpha = a$alpha, nlambda = a$nlambda)
+t <- proc.time() - x
+
+if (is.list(fit$beta)) {
+    message("is a list")
+    fit$beta <- do.call("rbind", lapply(fit$beta, as.matrix))
+} else {
+    message("not a list")
+}
+  
+writeMat("jssfigure4out.mat", a0 = fit$a0, beta = fit$beta, dev = fit$dev.ratio, 
+         df = fit$df, lambda = fit$lambda, nulldev = fit$nulldev, elapsed = t)
